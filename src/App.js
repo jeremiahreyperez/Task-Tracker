e75266fd2d26d5730b4b5a79f12ac71feeb2ff90
jeremiahreyperez <jreyperez@github.com>
2019-03-17 10:12:30 -0400

    Todo list complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,85 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/Layout/Header';
+import Todos from './components/Todo/Todos';
+import AddTodo from './components/Todo/AddTodo';
+import About from './components/Pages/About';
+import uuid from 'uuid';
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: []
+  };
+
+  componentDidMount() {
+    const storedTodos = localStorage.getItem('todos');
+    if (storedTodos) {
+      this.setState({ todos: JSON.parse(storedTodos) });
+      return;
+    } else {
+      localStorage.setItem('todos', [JSON.stringify(this.state.todos)]);
+    }
+  }
+
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+    // const list = this.state.todos;
+    // localStorage.setItem('todos', JSON.stringify(list));
+  };
+
+  deleteTodo = id => {
+    const list = [...this.state.todos];
+    const updatedList = list.filter(todo => todo.id !== id);
+
+    this.setState({ todos: updatedList });
+    localStorage.setItem('todos', JSON.stringify(updatedList));
+  };
+
+  addTodo = title => {
+    const newTodo = {
+      id: uuid.v4(),
+      title: title,
+      completed: false
+    };
+    const list = [...this.state.todos];
+    list.push(newTodo);
+    this.setState({ todos: list });
+    localStorage.setItem('todos', JSON.stringify(list));
+  };
+
   render() {
     return (
-      <div className="App">
-        <h1>To Do App</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route
+              exact
+              path="/"
+              render={props => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    deleteTodo={this.deleteTodo}
+                  />
+                </React.Fragment>
+              )}
+            />
+            <Route path="/about" component={About} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/Pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is a Todo List app made for practing React and React Router.</p>
+    </React.Fragment>
+  );
+}
+
+export default About;
