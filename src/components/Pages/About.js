469d803d17f81b44180807aa4044e501cbd1f8b9
Jeremiah Rey Perez <35105677+jeremiahreyperez@users.noreply.github.com>
2019-06-03 14:43:18 -0400

    Corrected misspelling in about page

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -4,7 +4,7 @@ function About() {
   return (
     <React.Fragment>
       <h1>About</h1>
-      <p>This is a Todo List app made for practing React and React Router.</p>
+      <p>This is a Todo List app made for practicing React and React Router.</p>
     </React.Fragment>
   );
 }
